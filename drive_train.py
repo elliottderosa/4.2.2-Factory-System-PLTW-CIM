dac36063a12f0c64c654a75f0342fc3846a98e28
Elliott DeRosa <76635411+elliottderosa@users.noreply.github.com>
2026-04-28 15:15:18 -0400

    Add SparkMaxGroup for managing motor groups
    Implement SparkMaxGroup class for motor control with duty cycle management.

---
+++ drive_train.py
@@ -0,0 +1,48 @@
+import time
+import can
+
+from sparkmax_can import SparkMax, HeartbeatTask
+
+class SparkMaxGroup:
+    def __init__(self, leader, followers, inverted=False):
+        self.leader = leader
+        self.followers = followers
+        self.inverted = inverted
+        self.last = None
+
+    def set_duty_cycle(self, value):
+        value = max(-1.0, min(1.0, value))
+
+        if self.inverted:
+            value = -value
+
+        if value == self.last:
+            return
+        self.last = value
+
+        self.leader.set_duty_cycle(value)
+        for f in self.followers:
+            f.set_duty_cycle(value)
+
+    def stop(self):
+        self.set_duty_cycle(0.0)
+
+bus = can.Bus(interface="socketcan", channel="can0")
+
+"""
+left = SparkMaxGroup(
+    SparkMax(bus, 1),
+    [SparkMax(bus, 2)]
+)
+
+right = SparkMaxGroup(
+    SparkMax(bus, 3),
+    [SparkMax(bus, 4)],
+    inverted=True
+)
+"""
+with HeartbeatTask(bus, enabled=True):
+    while True:
+        motor = SparkMax(bus, 1)
+        motor.set_duty_cycle(0.1)
+        time.sleep(0.02)
